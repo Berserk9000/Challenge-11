71e634d4cd2066ccee5292ffb05439ed4b172315
Berserk9000 <cruz.d.kevin@gmail.com>
2022-08-25 18:01:06 -0400

    working app with .get,.post,file sync added

--- server.js
+++ server.js
@@ -20,6 +20,23 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/inde.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let notelength = (noteList.length).toString();
+
+    
+    newNote.id = notelength;
+    
+    noteList.push(newNote);
+
+    
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+})
+
+//starting appp
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
