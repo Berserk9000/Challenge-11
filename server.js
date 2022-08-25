3ef831c09d479f8fc4915f3ab89c0a0a629d4db3
Berserk9000 <cruz.d.kevin@gmail.com>
2022-08-25 14:58:04 -0400

    cleaning up adding use

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
-
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const uniqid = require('uniqid');
+const PORT = process.env.PORT || 3001;
+const noteData = require('./db/db.json')
+
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
