94208532cbf00a577deb72617daba8bd797a8f33
Berserk9000 <cruz.d.kevin@gmail.com>
2022-08-25 15:27:40 -0400

    get setup

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/inde.html"));
+});
+
