15974fa15ef1b8c9da02637a08d9805aef103338
Berserk9000 <cruz.d.kevin@gmail.com>
2022-08-25 15:14:12 -0400

    workong on server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const uniqid = require('uniqid');
-const PORT = process.env.PORT || 3001;
-const noteData = require('./db/db.json')
+
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+
+
